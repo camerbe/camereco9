dbcb2aebcfb754d972cad34505349c185653c713
tristan <sgmb.belgique@gmail.com>
2023-01-19 18:55:03 +0100

    Add auth interceptor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthInterceptor } from './shared/auth.interceptor';
 import { AuthService } from './shared/auth.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/shared/auth.interceptor.ts
+++ frontend/src/app/shared/auth.interceptor.ts
@@ -6,13 +6,22 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private authservice:AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    request=request.clone({
+     setHeaders:{
+      'Access-Control-Allow-Origin':'*',
+      'Content-Type':'application/json',
+      'Authorization':`Bearer ${this.authservice.getToken()}.`,
+
+     }
+    });
     return next.handle(request);
   }
 }
